23c71e89f5f48f9afa0e521af50cfd6694c0317c
Lubov Flek <lubov.flek@yandex.ru>
2022-07-10 17:52:18 +0300

    initial commit

---
+++ src/main/java/ru/lubovflek/daysto/DaysToApplication.java
@@ -0,0 +1,13 @@
+package ru.lubovflek.daysto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaysToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaysToApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lubovflek/daysto/DaysToApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lubovflek.daysto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaysToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
